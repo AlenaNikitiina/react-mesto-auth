9371cd88e1faa164f9da9f7a5a4f4628b9f767e7
Alena Nikitina <alena.tkachenk@mail.ru>
2023-03-30 12:12:08 +0300

    style: header is mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,3 +27,28 @@ export default function Header( {userEmail, onSignOut } ) {
 
 /* навигация и ссылки  <NavLink to="/about-us" className={({isActive}) => `menu__link ${isActive ? "menu__link_active" : ""}`}>О нас</NavLink>*/
 
+/*
+export default function Header( {userEmail, onSignOut } ) {
+  return (
+    <header className="header">
+      <img className="header__logo" src={headerLogo} alt="логотип приложения Место" />
+      <Routes>
+        <Route path='/sign-in' element={
+          <Link to={'/sign-up'} className="header__navLink header__navLink_active">Регистрация</Link> }
+        />
+        <Route path='/sign-up' element={
+          <Link to={'/sign-in'} className="header__navLink header__navLink_active">Войти</Link> }
+        />
+        <Route path='/' element={
+          <>
+            <div className='header__container'>
+              <p className='header__email'>{userEmail}</p>
+              <button className='header__signOut' onClick={onSignOut}>Выйти</button>
+            </div>
+          </>}
+        />
+      </Routes>
+    </header>
+  );
+}
+*/
